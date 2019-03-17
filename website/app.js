df67485bb69bcd47025dbf4f3f2cddfc2d5d3ac4
trangsinh87 <trangsinh87@yahoo.com.vn>
2019-03-17 23:50:35 +0700

    update

--- website/app.js
+++ website/app.js
@@ -1,4 +1,9 @@
-const config = require('config');
+const config = require('config'),
+	express = require('express');
+var app = express();
 
+app.get('/', function(req, res) {
+	res.send('Hello World')
+});
 
-console.log(config);
+app.listen(3000);
